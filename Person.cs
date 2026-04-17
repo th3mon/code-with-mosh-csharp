5c5297adc35e122b69af532b3ab5296acfbc8d7a
th3mon <p.szelenberger@gmail.com>
2026-04-17 22:37:36 +0200

    Person

---
+++ Person.cs
@@ -0,0 +1,12 @@
+namespace HelloWorld;
+
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+    
+    public void Introduce()
+    {
+        Console.WriteLine("My name is {0} {1}", FirstName, LastName);
+    }
+}
