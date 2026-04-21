be78cd0197490a82d90938f54ff840d766a12eab
th3mon <p.szelenberger@gmail.com>
2026-04-21 04:03:31 +0200

    Turn off exercises

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿namespace HelloWorld;
+namespace HelloWorld;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ControlFlow.DivisibleByThree();
-        ControlFlow.NumberOrExit();
-        ControlFlow.CountFactorial();
-        ControlFlow.GuessTheNumber();
+        // ControlFlow.DivisibleByThree();
+        // ControlFlow.NumberOrExit();
+        // ControlFlow.CountFactorial();
+        // ControlFlow.GuessTheNumber();
 
-        ListsExercises.LikesCounter();
-        ListsExercises.ReverseName();
-        ListsExercises.GiveMeAFiveNumbers();
-        ListsExercises.GatherNumbers();
-        ListsExercises.PrintThreeSmallestNumbers();
+        // ListsExercises.LikesCounter();
+        // ListsExercises.ReverseName();
+        // ListsExercises.GiveMeAFiveNumbers();
+        // ListsExercises.GatherNumbers();
+        // ListsExercises.PrintThreeSmallestNumbers();
     }
 
     private static void PrintList<T>(List<T> list)
